5a260f7a4a3a943dfe42b123d719240f5506f7aa
Prajwal518 <prajwalgowda.s518@gmail.com>
2021-02-11 10:48:08 +0530

    added a login with facebook. added a dynamic secrets page with a modified submit page.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate')
 // //bcrypt
 // const bcrypt = require('bcrypt');
@@ -37,7 +38,9 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema ({
   email: String,
   password:String,
-  googleId: String
+  googleId: String,
+  facebookId:String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -71,10 +74,27 @@ function(accessToken, refreshToken, profile, cb) {
 }
 ));
 //oauth 
+//facebook
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_APP_ID,
+  clientSecret: process.env.FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/secrets",
+  profileFields: ['id', 'displayName', 'photos', 'email']
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+//facebook
+
 app.get("/", function(req,res){
   res.render("home");
 });
 
+// google and facebook
 app.get("/auth/google",
   passport.authenticate("google", { scope: ["profile"] })
   );
@@ -83,8 +103,22 @@ app.get("/auth/google/secrets",
 passport.authenticate("google", { failureRedirect: "/login "}),
 function(req, res) {
   // Successful authentication, redirect home.
-  res.redirect('/');
+  res.redirect('/secrets');
 }); 
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook', {scope: ['email']})
+  );
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+//google and facebook
+
 app.get("/login", function(req,res){
   res.render("login");
 });
@@ -112,11 +146,40 @@ app.get("/register", function(req,res){
 });
 
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUsers){
+        res.render("secrets", {UsersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }else{
     res.redirect("/login");
   }
+});
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        foundUser.secret=submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 });
 
 app.get("/logout", function(req, res){
